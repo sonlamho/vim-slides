49e1ecf5728448e98dd2703125cc0b3e4b65cc6c
Lam Son Ho <holamson2000@gmail.com>
2020-10-01 22:57:30 +0700

    apply black

--- create_slide_deck.py
+++ create_slide_deck.py
@@ -7,24 +7,24 @@ DEFAULT_LINES = 20
 
 
 def get_slides(fpath: str, n_lines: int) -> List[Tuple[str, str]]:
-    with open(fpath, 'r') as f:
+    with open(fpath, "r") as f:
         lines = f.readlines()
     #
     slides = []
-    ext = ''
+    ext = ""
     texts: List[str] = []
     for line in lines:
-        if line.startswith('##'):
+        if line.startswith("##"):
             if texts:
                 # create a slide
                 post_process(texts)
                 assert len(texts) <= n_lines
-                padding = ['\n'] * (n_lines - len(texts))
-                slides.append((ext, ''.join(texts + padding)))
+                padding = ["\n"] * (n_lines - len(texts))
+                slides.append((ext, "".join(texts + padding)))
             # prep for a new slide
-            ext = line.strip('#').strip()
-            if not ext.startswith('.'):
-                ext = '.' + ext
+            ext = line.strip("#").strip()
+            if not ext.startswith("."):
+                ext = "." + ext
             texts = []
         else:
             texts.append(line)
@@ -33,15 +33,15 @@ def get_slides(fpath: str, n_lines: int) -> List[Tuple[str, str]]:
 
 def post_process(slide_lines: List[str]):
     if slide_lines[0].strip():
-        slide_lines.insert(0, '\n')
+        slide_lines.insert(0, "\n")
 
 
 def export(slides: List[Tuple[str, str]], fpath: str):
     for i, ext_text in enumerate(slides):
         ext, text = ext_text
         num = str(i).zfill(8)
-        slide_path = os.path.join(fpath, f'slide_{num}{ext}')
-        mode = 'w' if os.path.exists(slide_path) else 'x'
+        slide_path = os.path.join(fpath, f"slide_{num}{ext}")
+        mode = "w" if os.path.exists(slide_path) else "x"
         with open(slide_path, mode) as f:
             f.write(text)
 
@@ -49,25 +49,26 @@ def export(slides: List[Tuple[str, str]], fpath: str):
 def main(args: Namespace):
     fpath = os.path.realpath(args.folderpath[0])
     print(f"Creating slides in:\n  {fpath}")
-    src_path = os.path.join(fpath, 'source.py')
+    src_path = os.path.join(fpath, "source.py")
     slides = get_slides(src_path, args.lines)
     export(slides, fpath)
 
 
 def _init_parser() -> argparse.ArgumentParser:
     _parser = argparse.ArgumentParser()
-    _parser.add_argument('folderpath', type=str, nargs=1)
-    _parser.add_argument('--lines', type=int, default=DEFAULT_LINES,
-                         help="Target number of lines per slide")
+    _parser.add_argument("folderpath", type=str, nargs=1)
+    _parser.add_argument(
+        "--lines",
+        type=int,
+        default=DEFAULT_LINES,
+        help="Target number of lines per slide",
+    )
     return _parser
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     _parser = _init_parser()
     args: Namespace = _parser.parse_args()
-    args_dict = {
-        a: getattr(args, a)
-        for a in dir(args) if not a.startswith('_')
-    }
+    args_dict = {a: getattr(args, a) for a in dir(args) if not a.startswith("_")}
     print(args_dict)
     main(args)
